b07daf6a52a73633f59a4b1856d495884f032183
wiselyku <wiselyku@gmail.com>
2020-12-22 21:55:36 +0000

    add a new feature log message in the page

--- hello/urls.py
+++ hello/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("hello/<name>", views.hello_there, name="hello_there"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),    
+    path("log/", views.log_message, name="log"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -24,3 +24,21 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+# Add these to existing imports at the top of the file:
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+
+# Add this code elsewhere in the file:
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
